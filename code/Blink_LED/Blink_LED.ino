cbf5f31de14a24949c9fcc7ce782531e658cc952
Rui Santos <ee11077@fe.up.pt>
2017-12-15 10:51:19 +0000

    Add files via upload

---
+++ code/Blink_LED/Blink_LED.ino
@@ -0,0 +1,20 @@
+/*
+  Blink
+*/
+
+// ledPin refers to ESP32 GPIO 23
+const int ledPin = 23;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin ledPin as an output.
+  pinMode(ledPin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                  // wait for a second
+  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                  // wait for a second
+}
