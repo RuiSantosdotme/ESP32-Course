e0b18584d387cddc2a600626cd2248f74cbe9397
Rui Santos <hello@ruisantos.me>
2017-12-23 11:37:12 +0000

    touchRead function

---
+++ code/Touch_Sensitive_LED/Touch_Sensitive_LED.ino
@@ -0,0 +1,34 @@
+// set pin numbers
+#define touchPin  T0 
+const int ledPin = 16;
+
+// change with your threshold value
+const int threshold = 20;
+// variable for storing the touch pin value 
+int touchValue;
+
+void setup(){
+  Serial.begin(115200);
+  delay(1000); // give me time to bring up serial monitor
+  // initialize the LED pin as an output:
+  pinMode (ledPin, OUTPUT);
+}
+
+void loop(){
+  // read the state of the pushbutton value:
+  touchValue = touchRead(touchPin);
+  Serial.print(touchValue);
+  // check if the touchValue is below the threshold
+  // if it is, set ledPin to HIGH
+  if(touchValue < threshold){
+    // turn LED on
+    digitalWrite(ledPin, HIGH);
+    Serial.println(" - LED on");
+  }
+  else{
+    // turn LED off
+    digitalWrite(ledPin, LOW);
+    Serial.println(" - LED off");
+  }
+  delay(500);
+}
