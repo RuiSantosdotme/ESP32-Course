6a9683e8c6dc3cca6f11d7dd51dd0823ec46c069
Rui Santos <ee11077@fe.up.pt>
2018-04-06 11:25:51 +0100

    comment fixed

--- code/Servo/Sweep/Sweep.ino
+++ code/Servo/Sweep/Sweep.ino
@@ -12,7 +12,7 @@ Servo myservo;  // create servo object to control a servo
 int pos = 0;    // variable to store the servo position
 
 void setup() {
-  myservo.attach(13);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(13);  // attaches the servo on pin 13 to the servo object
 }
 
 void loop() {
