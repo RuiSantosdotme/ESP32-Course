3e843abb729df7f0540dc91a7c5754ea19e4bd2e
Rui Santos <randomnerdtutorials@gmail.com>
2019-01-07 23:23:39 +0000

    Fixed OLED display not initializing

--- code/BLE_Client_OLED/BLE_Client_OLED.ino
+++ code/BLE_Client_OLED/BLE_Client_OLED.ino
@@ -43,10 +43,12 @@ static BLERemoteCharacteristic* humidityCharacteristic;
 const uint8_t notificationOn[] = {0x1, 0x0};
 const uint8_t notificationOff[] = {0x0, 0x0};
 
-#define OLED_RESET 16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
-//SSD1306 Display
-Adafruit_SSD1306 display(OLED_RESET);
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 //Connect to the BLE Server that has the name, Service, and Characteristics
 bool connectToServer(BLEAddress pAddress) {
@@ -124,8 +126,12 @@ static void humidityNotifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteri
 
 void setup() {
   //OLED display setup
-  Wire.begin();
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  
   display.clearDisplay();
   display.setTextSize(1);
   //display.setBackgroundcolor(BLACK);
