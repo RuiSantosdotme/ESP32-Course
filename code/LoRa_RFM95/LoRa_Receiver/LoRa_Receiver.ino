a709e036cb078bcf865c42e82a3e67207b514df2
Rui Santos <randomnerdtutorials@gmail.com>
2019-01-07 23:22:24 +0000

    Fixed OLED display not initializing

--- code/LoRa_RFM95/LoRa_Receiver/LoRa_Receiver.ino
+++ code/LoRa_RFM95/LoRa_Receiver/LoRa_Receiver.ino
@@ -14,13 +14,20 @@
 #define rst 14
 #define dio0 2
 
-#define OLED_RESET 16
-Adafruit_SSD1306 display(OLED_RESET);
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() { 
-  //OLED display setup
-  Wire.begin();
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
