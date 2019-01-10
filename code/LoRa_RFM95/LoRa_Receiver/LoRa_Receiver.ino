8495c1a4af1eedc341192ed4c66309a3fa54b2b8
Rui Santos <randomnerdtutorials@gmail.com>
2019-01-10 16:02:42 +0000

    height 64px

--- code/LoRa_RFM95/LoRa_Receiver/LoRa_Receiver.ino
+++ code/LoRa_RFM95/LoRa_Receiver/LoRa_Receiver.ino
@@ -15,7 +15,7 @@
 #define dio0 2
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
