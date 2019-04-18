f1fb8aa92e65254243a3eefc991c3a99f870bced
Rui Santos <randomnerdtutorials@gmail.com>
2019-04-18 15:08:55 +0100

    Update Blink_Without_Delay.ino

--- code/Blink_Without_Delay/Blink_Without_Delay.ino
+++ code/Blink_Without_Delay/Blink_Without_Delay.ino
@@ -14,7 +14,7 @@ int ledState = LOW;             // ledState used to set the LED
 unsigned long previousMillis = 0;        // will store last time LED was updated
 
 // constants won't change :
-const long interval = 5000;           // interval at which to blink (milliseconds)
+const long interval = 1000;           // interval at which to blink (milliseconds)
 
 void setup() {
   // set the digital pin as output:
