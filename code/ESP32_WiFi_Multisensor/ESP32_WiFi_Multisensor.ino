c1b7068c9855b4aac29b58710493358d36e1dcdb
Rui Santos <randomnerdtutorials@gmail.com>
2020-05-19 17:37:24 +0100

    Update ESP32_WiFi_Multisensor.ino

--- code/ESP32_WiFi_Multisensor/ESP32_WiFi_Multisensor.ino
+++ code/ESP32_WiFi_Multisensor/ESP32_WiFi_Multisensor.ino
@@ -68,6 +68,13 @@ String header;
 // Set web server port number to 80
 WiFiServer server(80);
 
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
 void setup() {
   // initialize the DHT sensor
   dht.begin();
@@ -132,9 +139,12 @@ void setup() {
 void loop() {
   WiFiClient client = server.available();   // Listen for incoming clients
   if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
     Serial.println("New Client.");          // print a message out in the serial port
     String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
         Serial.write(c);                    // print it out the serial monitor
