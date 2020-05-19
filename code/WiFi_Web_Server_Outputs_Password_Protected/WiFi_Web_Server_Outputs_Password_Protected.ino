95c2e70352664d64ed30d80af756fbed5d6e5d7e
Rui Santos <randomnerdtutorials@gmail.com>
2020-05-19 17:17:40 +0100

    Update WiFi_Web_Server_Outputs_Password_Protected.ino

--- code/WiFi_Web_Server_Outputs_Password_Protected/WiFi_Web_Server_Outputs_Password_Protected.ino
+++ code/WiFi_Web_Server_Outputs_Password_Protected/WiFi_Web_Server_Outputs_Password_Protected.ino
@@ -7,7 +7,7 @@
 #include <WiFi.h>
 
 // Replace with your network credentials
-const char* ssid     = "REPLACE_WITH_YOUR_SSID";
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
 const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 
 // Set web server port number to 80
@@ -23,6 +23,13 @@ String output27State = "off";
 // Assign output variables to GPIO pins
 const int output26 = 26;
 const int output27 = 27;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
 
 void setup() {
   Serial.begin(115200);
@@ -53,9 +60,12 @@ void loop(){
   WiFiClient client = server.available();   // Listen for incoming clients
 
   if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
     Serial.println("New Client.");          // print a message out in the serial port
     String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
         Serial.write(c);                    // print it out the serial monitor
