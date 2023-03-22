50c9ae1f473956f8b89552099006cc20efa5bd07
Rui Santos <randomnerdtutorials@gmail.com>
2023-03-22 13:43:34 +0000

    Update PIR_Interrupts_Timers.ino

--- code/PIR_Interrupts_Timers/PIR_Interrupts_Timers.ino
+++ code/PIR_Interrupts_Timers/PIR_Interrupts_Timers.ino
@@ -13,10 +13,10 @@ const int motionSensor = 27;
 unsigned long now = millis();
 unsigned long lastTrigger = 0;
 boolean startTimer = false;
+boolean motion = false;
 
 // Checks if motion was detected, sets LED HIGH and starts a timer
 void IRAM_ATTR detectsMovement() {
-  Serial.println("MOTION DETECTED!!!");
   digitalWrite(led, HIGH);
   startTimer = true;
   lastTrigger = millis();
@@ -39,10 +39,15 @@ void setup() {
 void loop() {
   // Current time
   now = millis();
+  if((digitalRead(led) == HIGH) && (motion == false)) {
+    Serial.println("MOTION DETECTED!!!");
+    motion = true;
+  }
   // Turn off the LED after the number of seconds defined in the timeSeconds variable
   if(startTimer && (now - lastTrigger > (timeSeconds*1000))) {
     Serial.println("Motion stopped...");
     digitalWrite(led, LOW);
     startTimer = false;
+    motion = false;
   }
 }
