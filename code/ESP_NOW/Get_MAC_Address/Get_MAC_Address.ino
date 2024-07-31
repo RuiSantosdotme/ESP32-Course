fb2b310d7fc5f5d6b2f34ba03f80a5b3b34bee91
Rui Santos <randomnerdtutorials@gmail.com>
2024-07-31 12:18:06 +0100

    Update Get_MAC_Address.ino

--- code/ESP_NOW/Get_MAC_Address/Get_MAC_Address.ino
+++ code/ESP_NOW/Get_MAC_Address/Get_MAC_Address.ino
@@ -3,12 +3,29 @@
   Complete project details at https://RandomNerdTutorials.com
 *********/
 
-#include "WiFi.h"
- 
+#include <WiFi.h>
+#include <esp_wifi.h>
+
+void readMacAddress(){
+  uint8_t baseMac[6];
+  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+  if (ret == ESP_OK) {
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                  baseMac[0], baseMac[1], baseMac[2],
+                  baseMac[3], baseMac[4], baseMac[5]);
+  } else {
+    Serial.println("Failed to read MAC address");
+  }
+}
+
 void setup(){
   Serial.begin(115200);
-  WiFi.mode(WIFI_MODE_STA);
-  Serial.println(WiFi.macAddress());
+
+  WiFi.mode(WIFI_STA);
+  WiFi.STA.begin();
+
+  Serial.print("[DEFAULT] ESP32 Board MAC Address: ");
+  readMacAddress();
 }
  
 void loop(){
