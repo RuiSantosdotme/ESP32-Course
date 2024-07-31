d0c3969056e381160f6b30b3ea2de39ede1377ae
Rui Santos <randomnerdtutorials@gmail.com>
2024-07-31 11:40:43 +0100

    Update LED_PWM_Example_1.ino

--- code/LED_PWM_Example_1/LED_PWM_Example_1.ino
+++ code/LED_PWM_Example_1/LED_PWM_Example_1.ino
@@ -3,29 +3,28 @@ const int ledPin = 16;  // 16 corresponds to GPIO16
 
 // setting PWM properties
 const int freq = 5000;
-const int ledChannel = 0;
 const int resolution = 8;
  
 void setup(){
-  // configure LED PWM functionalitites
-  ledcSetup(ledChannel, freq, resolution);
+  // configure LED PWM
+  ledcAttach(ledPin, freq, resolution);
   
-  // attach the channel to the GPIO to be controlled
-  ledcAttachPin(ledPin, ledChannel);
+  // if you want to attach a specific channel, use the following instead
+  //ledcAttachChannel(ledPin, freq, resolution, 0);
 }
  
 void loop(){
   // increase the LED brightness
   for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
     // changing the LED brightness with PWM
-    ledcWrite(ledChannel, dutyCycle);
+    ledcWrite(ledPin, dutyCycle);
     delay(15);
   }
 
   // decrease the LED brightness
   for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
     // changing the LED brightness with PWM
-    ledcWrite(ledChannel, dutyCycle);   
+    ledcWrite(ledPin, dutyCycle);   
     delay(15);
   }
 }
