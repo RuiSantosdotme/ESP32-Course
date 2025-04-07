69408933bd7ffb75fc68799465a458885e95ee11
Rui Santos <randomnerdtutorials@gmail.com>
2025-04-07 10:23:40 +0100

    Update TimerWakeUp.ino

--- code/DeepSleep/TimerWakeUp/TimerWakeUp.ino
+++ code/DeepSleep/TimerWakeUp/TimerWakeUp.ino
@@ -1,33 +1,14 @@
-/*
-Simple Deep Sleep with Timer Wake Up
-=====================================
-ESP32 offers a deep sleep mode for effective power
-saving as power is an important factor for IoT
-applications. In this mode CPUs, most of the RAM,
-and all the digital peripherals which are clocked
-from APB_CLK are powered off. The only parts of
-the chip which can still be powered on are:
-RTC controller, RTC peripherals ,and RTC memories
+/* Simple Deep Sleep with Timer Wake Up
+   ESP32 offers a deep sleep mode for effective power saving as power is an important factor for IoT applications. In this mode CPUs, most of the RAM, and all the digital peripherals which are clocked
+   from APB_CLK are powered off. The only parts of the chip which can still be powered on are: RTC controller, RTC peripherals ,and RTC memories This code displays the most basic deep sleep with a timer to wake it up and how to store data in RTC memory to use it over reboots This code is under Public Domain License.
+   Author: Pranav Cherukupalli <cherukupallip@gmail.com> */
 
-This code displays the most basic deep sleep with
-a timer to wake it up and how to store data in
-RTC memory to use it over reboots
-
-This code is under Public Domain License.
-
-Author:
-Pranav Cherukupalli <cherukupallip@gmail.com>
-*/
-
-#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  5        /* Time ESP32 will go to sleep (in seconds) */
+#define uS_TO_S_FACTOR 1000000ULL  // Conversion factor for micro seconds to seconds
+#define TIME_TO_SLEEP  5           // Time ESP32 will go to sleep (in seconds)
 
 RTC_DATA_ATTR int bootCount = 0;
 
-/*
-Method to print the reason by which ESP32
-has been awaken from sleep
-*/
+// Method to print the reason by which ESP32 has been awaken from sleep
 void print_wakeup_reason(){
   esp_sleep_wakeup_cause_t wakeup_reason;
 
@@ -46,19 +27,16 @@ void print_wakeup_reason(){
 
 void setup(){
   Serial.begin(115200);
-  delay(1000); //Take some time to open up the Serial Monitor
+  delay(1000); // Take some time to open up the Serial Monitor
 
-  //Increment boot number and print it every reboot
+  // Increment boot number and print it every reboot
   ++bootCount;
   Serial.println("Boot number: " + String(bootCount));
 
-  //Print the wakeup reason for ESP32
+  // Print the wakeup reason for ESP32
   print_wakeup_reason();
 
-  /*
-  First we configure the wake up source
-  We set our ESP32 to wake up every 5 seconds
-  */
+  // First we configure the wake up source We set our ESP32 to wake up every 5 seconds
   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
   Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
   " Seconds");
@@ -75,14 +53,8 @@ void setup(){
   //esp_deep_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
   //Serial.println("Configured all RTC Peripherals to be powered down in sleep");
 
-  /*
-  Now that we have setup a wake cause and if needed setup the
-  peripherals state in deep sleep, we can now start going to
-  deep sleep.
-  In the case that no wake up sources were provided but deep
-  sleep was started, it will sleep forever unless hardware
-  reset occurs.
-  */
+  // Now that we have setup a wake cause and if needed setup the peripherals state in deep sleep, we can now start going to deep sleep.
+  // In the case that no wake up sources were provided but deep sleep was started, it will sleep forever unless hardware reset occurs.
   Serial.println("Going to sleep now");
   delay(1000);
   Serial.flush(); 
@@ -91,5 +63,5 @@ void setup(){
 }
 
 void loop(){
-  //This is not going to be called
+  // This is not going to be called
 }
